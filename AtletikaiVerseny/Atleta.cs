443699c2e74961cb44192a43e9dfec5a7badda76
ViczianGabor <viczian.gabor13@gmail.com>
2020-10-13 08:54:52 +0200

    Kész

---
+++ AtletikaiVerseny/Atleta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtletikaiVerseny
+{
+    class Atleta
+    {
+        public string Rajtszam { get; private set; }
+
+        public string VezNev { get; private set; }
+
+        public string KerNev { get; private set; }
+
+        public string Egyesulet { get; private set; }
+
+        public int Ugras { get; private set; }
+
+
+
+
+        public Atleta(string szoveg)
+        {
+            string[] adat = szoveg.Split(';');
+            
+            Rajtszam = adat[0];
+            VezNev = adat[1];
+            Egyesulet = adat[2];
+            Ugras = int.Parse(adat[3]);
+            
+        }
+
+
+
+
+
+
+
+    }
+}
--- AtletikaiVerseny/Program.cs
+++ AtletikaiVerseny/Program.cs
@@ -3,14 +3,120 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AtletikaiVerseny
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<Atleta> atletak = new List<Atleta>();
+
+        static void Beolvas()
         {
+            Console.WriteLine("1. feladat: adatok beolvasása");
+            StreamReader file = new StreamReader("tavol.csv");
+            while (!file.EndOfStream)
+            {
+                atletak.Add(new Atleta(Console.ReadLine()));
+            }
+
+            file.Close();
 
         }
+
+        static void feladat2()
+        {
+            Console.WriteLine("\n2. feladat: Nevek és ugrások");
+            foreach (var item in atletak)
+            {
+                Console.WriteLine("{0,10} {-1,10}",item.VezNev,item.Ugras);
+            }
+
+        }
+
+
+        static void feladat3()
+        {
+            Console.WriteLine("\n3. feladat: Egyesületek:");
+            List<string> egyesuletek = new List<string>();
+            foreach (var e in atletak)
+            {
+                if (!egyesuletek.Contains(e.Egyesulet))
+                {
+                    egyesuletek.Add(e.Egyesulet);
+                }
+            }
+
+            for (int i = 0; i < egyesuletek.Count; i++)
+            {
+                Console.WriteLine(egyesuletek[i]);
+            }
+        }
+
+        static void feladat4()
+        {
+            Console.WriteLine("\n4. feladat: Legnagyobb ugrása:");
+            int max = atletak[0].Ugras;
+            int index = 0;
+            for (int i = 0; i < atletak.Count; i++)
+            {
+                if (max < atletak[i].Ugras)
+                {
+                    max = atletak[i].Ugras;
+                    index = i;
+                }
+            }
+            
+
+            
+
+            Console.WriteLine(atletak[index].VezNev+": "+max+"cm" );
+        }
+
+        static void feladat5()
+        {
+            Console.Write("\n5.feladat: ");
+            double osszeg = 0;
+            for (int i = 0; i < atletak.Count; i++)
+            {
+                osszeg += (atletak[i].Ugras);
+            }
+            double atlag = osszeg / atletak.Count;
+            int db = 0;
+            for (int i = 0; i < atletak.Count; i++)
+            {
+                if (atletak[i].Ugras < atlag)
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine("Átlag alatti ugrások száma: "+db);
+
+
+        }
+
+        static void feladat6()
+        {
+            StreamWriter sw = new StreamWriter("versenyzok.txt");
+            for (int i = 0; i < atletak.Count; i++)
+            {
+                sw.WriteLine(atletak[i].Rajtszam + ";" + atletak[i].VezNev);
+            }
+
+            sw.Close();
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Beolvas();
+            feladat2();
+            feladat3();
+            feladat4();
+            feladat5();
+            feladat6();
+            Console.ReadKey();
+        }  
     }
 }
